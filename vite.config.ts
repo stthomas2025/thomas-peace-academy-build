0f8f2416cc171438dc650954f190ebfba1b37587
stthomas2025 <nlstthomasnep@gmail.com>
2025-04-21 14:40:10 +0545

    Update vite.config.ts
    for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
+// 👇 Replace 'your-repo-name' with your actual GitHub repo name
+const repoName = 'thomas-peace-academy-build';
+
 export default defineConfig(({ mode }) => ({
+  base: `/${thomas-peace-academy-build}/`, // ← ADD THIS LINE
   server: {
     host: "::",
     port: 5173,
